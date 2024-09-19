e05dd36fb8f35e7d238c93ab392cca7ea3b8a814
faraygd <102520149+faraygd@users.noreply.github.com>
2024-09-19 17:40:12 +0700

    fix: unused

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Certificate from "@/components/certificate";
 import Contacts from "@/components/contacts";
 import Footer from "@/components/footer";
 import Header from "@/components/header";
@@ -15,7 +14,6 @@ export default function Home() {
       <Header />
       <Projects />
       <Work />
-      <Certificate />
       <Contacts />
       <Footer />
     </main>
