068a64b13944678d640bb10a4140183d531bdfd3
faraygd <102520149+faraygd@users.noreply.github.com>
2024-09-19 21:52:25 +0700

    add: underline and cursor for footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <hr className="text-white w-full" />
         <div className="flex justify-between mt-8 mb-4">
           <h1>© 2024 - Aldi Ahmad Fahrizi Ilmawan</h1>
-          <p>Privacy Policy</p>
+          <p className="hover:underline hover:cursor-pointer">Privacy Policy</p>
         </div>
       </div>
     </footer>
