8bdaceb1a28c544cca868c30b77c7444610d1559
aldifhrent <aldifahriziilmawan@gmail.com>
2024-11-12 16:47:48 +0700

    first commit

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -1,10 +1,11 @@
 import * as z from "zod";
 
 export const workSchema = z.object({
-  place: z.string().min(1, "Place is required"),
-  role: z.string().min(1, "Role is required"),
+  workPlace: z.string(),
+  role: z.string(),
   startDate: z.date(),
   endDate: z.date(),
+  description: z.string().array(),
 });
 
 export type WorkFormValues = z.infer<typeof workSchema>;
